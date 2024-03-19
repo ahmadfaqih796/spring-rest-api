f76bfb83b92d3545534ca0c2cfec964ceac240ee
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-03-19 20:46:08 +0700

    fitur search by name

--- src/main/java/com/latihan/demoapi/models/repository/ProductRepo.java
+++ src/main/java/com/latihan/demoapi/models/repository/ProductRepo.java
@@ -1,9 +1,12 @@
 package com.latihan.demoapi.models.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.latihan.demoapi.models.entities.Product;
 
 public interface ProductRepo extends CrudRepository<Product, Long> {
-
+   // Buat fitur search
+   List<Product> findByNameContains(String name);
 }
--- src/main/java/com/latihan/demoapi/services/ProductService.java
+++ src/main/java/com/latihan/demoapi/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.latihan.demoapi.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,5 +34,9 @@ public class ProductService {
    @SuppressWarnings("null")
    public void removeOne(Long id) {
       productRepo.deleteById(id);
+   }
+
+   public List<Product> findByName(String name) {
+      return productRepo.findByNameContains(name);
    }
 }
