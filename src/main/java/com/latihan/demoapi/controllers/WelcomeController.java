b6540c153b44c5aa7bbcdd093129650f7af4dcee
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-03-19 20:19:20 +0700

    product entity

--- src/main/java/com/latihan/demoapi/controllers/WelcomeController.java
+++ src/main/java/com/latihan/demoapi/controllers/WelcomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/welcome")
 public class WelcomeController {
 
-   @GetMapping
+   @GetMapping()
    public String welcome() {
       return "Welcome to Spring Boot";
    }
